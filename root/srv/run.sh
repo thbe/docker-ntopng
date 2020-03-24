0e8038f0d3cf26fc82c007952dd868616741cd8f
thbe <project@bendler-net.de>
2020-03-24 23:42:03 +0100

    Incorporation of latest fritzdump.sh changes
    Signed-off-by: thbe <project@bendler-net.de>

--- root/srv/run.sh
+++ root/srv/run.sh
@@ -45,7 +45,8 @@ if [ -n "${NTOPNG_ENV_FRITZBOX_CAPTURE}" ]; then
   ### Get FritzBox capture interface (wan/lan) ###
   if [ -n "${NTOPNG_ENV_FRITZBOX_IFACE}" ]; then
     if [ "${NTOPNG_ENV_FRITZBOX_IFACE}" = "wan" ]; then
-      FRITZBOX_IFACE="3-17"
+      # FRITZBOX_IFACE="3-17"
+      FRITZBOX_IFACE="2-0"
     else
       FRITZBOX_IFACE="1-lan"
     fi
@@ -56,19 +57,32 @@ if [ -n "${NTOPNG_ENV_FRITZBOX_CAPTURE}" ]; then
     FRITZBOX_PASSWORD=${NTOPNG_ENV_FRITZBOX_PASSWORD}
   fi
 
+  ### Create FritzBox SID file ###
+  FRITZBOX_SIDFILE="/tmp/fritz.sid"
+  if [ ! -f ${FRITZBOX_SIDFILE} ]; then
+    touch ${FRITZBOX_SIDFILE}
+  fi
+  FRITZBOX_SID=$(cat ${FRITZBOX_SIDFILE})
+
   ### Request FritzBox challenge token ###
-  FRITZBOX_CHALLENGE=$(curl -s http://fritz.box/login_sid.lua |  grep -o "<Challenge>[a-z0-9]\{8\}" | cut -d'>' -f2)
+  FRITZBOX_CHALLENGE=$(curl -k -s http://fritz.box/login_sid.lua |  grep -o "<Challenge>[a-z0-9]\{8\}" | cut -d'>' -f 2)
 
   ### Create FritzBox password hash ###
   FRITZBOX_HASH=$(perl -MPOSIX -e '
-      use Digest::MD5 "md5_hex";
-      my $ch_Pw = "$ARGV[0]-$ARGV[1]";
-      $ch_Pw =~ s/(.)/$1 . chr(0)/eg;
-      my $md5 = lc(md5_hex($ch_Pw));
-      print $md5;
-    ' -- "${FRITZBOX_CHALLENGE}" "${FRITZBOX_PASSWORD}")
-  FRITZBOX_SID=$(curl -s "http://fritz.box/login_sid.lua" -d "response=${FRITZBOX_CHALLENGE}-${FRITZBOX_HASH}" \
-                      -d 'username=' | grep -o "<SID>[a-z0-9]\{16\}" | cut -d'>' -f2)
+    use Digest::MD5 "md5_hex";
+    my $ch_Pw = "$ARGV[0]-$ARGV[1]";
+    $ch_Pw =~ s/(.)/$1 . chr(0)/eg;
+    my $md5 = lc(md5_hex($ch_Pw));
+    print $md5;
+  ' -- "${FRITZBOX_CHALLENGE}" "${FRITZBOX_PASSWORD}")
+  curl -k -s "http://fritz.box/login_sid.lua" -d "response=${FRITZBOX_CHALLENGE}-${FRITZBOX_HASH}" -d 'username=dslf-config' | grep -o "<SID>[a-z0-9]\{16\}" | cut -d'>' -f 2 > ${FRITZBOX_SIDFILE}
+  FRITZBOX_SID=$(cat ${FRITZBOX_SIDFILE})
+
+  ### Check if FritzBox authentification was successful ###
+  if [[ ${FRITZBOX_SID} =~ ^0+$ ]]; then
+    echo "Login failed! Fallback to normal startup"
+    unset ${NTOPNG_ENV_FRITZBOX_CAPTURE}
+  fi
 fi
 
 ### Display NTOPNG connection parameter ###
@@ -94,10 +108,10 @@ EOF
 /usr/bin/redis-server /etc/redis/redis.conf
 
 ### Start the NTOPNG instance ###
-NTOPNG_COMMAND="/usr/sbin/ntopng --dns-mode 1"
-FRITZBOX_URL="http://${FRITZBOX_IP}/cgi-bin/capture_notimeout?ifaceorminor=${FRITZBOX_IFACE}&snaplen=&capture=Start&sid=${FRITZBOX_SID}"
+NTOPNG_COMMAND="/usr/sbin/ntopng"
+FRITZBOX_URL="http://fritz.box//cgi-bin/capture_notimeout?ifaceorminor=${FRITZBOX_IFACE}\&snaplen=\&capture=Start\&sid=${FRITZBOX_SID}"
 if [ -n "${NTOPNG_ENV_FRITZBOX_CAPTURE}" ]; then
-  wget -qO- ${FRITZBOX_URL} | ${NTOPNG_COMMAND} -i -
+  wget --no-check-certificate -qO- ${FRITZBOX_URL} | ${NTOPNG_COMMAND} -i -
 else
   ${NTOPNG_COMMAND}
 fi
